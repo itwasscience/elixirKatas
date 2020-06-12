31a90723fd5e1d6b7f6eca0ad84e11bbee61c2cf
Zoey Fahner <zoey.fahner@segra.com>
2020-06-12 18:35:22 -0400

    mix format

--- lib/fizzbuzz.ex
+++ lib/fizzbuzz.ex
@@ -1,10 +1,11 @@
 defmodule KataFizzbuzz do
-    def calculate(count) do
-        Enum.map(1..count, &result/1)
-        |> Enum.join("\n")
-    end
-    defp result(x) when rem(x, 3) == 0 and rem(x, 5) == 0, do: "FizzBuzz"
-    defp result(x) when rem(x, 3) == 0, do: "Fizz"
-    defp result(x) when rem(x, 5) == 0, do: "Buzz"
-    defp result(x), do: to_string(x)
+  def calculate(count) do
+    Enum.map(1..count, &result/1)
+    |> Enum.join("\n")
+  end
+
+  defp result(x) when rem(x, 3) == 0 and rem(x, 5) == 0, do: "FizzBuzz"
+  defp result(x) when rem(x, 3) == 0, do: "Fizz"
+  defp result(x) when rem(x, 5) == 0, do: "Buzz"
+  defp result(x), do: to_string(x)
 end
